5bf620758347ec25797f605fb693055fc8846740
Тима <60360610+Tima-gaz@users.noreply.github.com>
2020-10-08 23:42:22 +0300

    Задание 7

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import MainScreen from "../main-screen/main-screen";
+import {Route, Switch, BrowserRouter} from "react-router-dom";
 import PropTypes from "prop-types";
+import AuthScreen from "../auth-screen/auth-screen";
+import RoomScreen from "../room-screen/room-screen";
+import Favorites from "../favorites/favorites";
 
 const App = (props) => {
   const {placesAmount} = props;
 
   return (
-    <MainScreen placesAmount={placesAmount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen placesAmount={placesAmount} />
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+        <Route exact path="/offer/:id">
+          <RoomScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
